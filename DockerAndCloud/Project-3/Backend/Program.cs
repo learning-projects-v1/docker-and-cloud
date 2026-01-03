3fa1cdf88428fc51bc5613e933331a35640cb8f5
Jakir <zakirsust2017@gmail.com>
2026-01-03 11:20:04 +0600

    dont load log for now

--- DockerAndCloud/Project-3/Backend/Program.cs
+++ DockerAndCloud/Project-3/Backend/Program.cs
@@ -16,8 +16,8 @@ builder.Services.AddCors((options) =>
 
 builder.Logging.ClearProviders();
 builder.Logging.AddProvider(new CustomLoggerProvider());
-var prodLogPath = config["Logging:LogPath"];
-builder.Logging.AddProvider(new FileLoggerProvider(prodLogPath));
+// var prodLogPath = config["Logging:LogPath"];
+// builder.Logging.AddProvider(new FileLoggerProvider(prodLogPath));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
